57105c3babfadd55338d80ea7765ab3d9db0d7e3
Guilherme Janke <guilhermejanke15@gmail.com>
2020-12-10 14:51:40 -0300

    v1
    V1

---
+++ Cada n° em uma linha.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//Faça um programa que apresente os números de 1 a 20, cada número em uma linha.
+
+
+int main()
+{
+    int i; 
+    for (i=1;i<=20;i++){
+    	printf("%d\n",i);
+	}
+}
